dd8a10e7e845d57ad58e676670057f29b095974a
Khanas Vasily <alcoapcio@gmail.com>
2016-12-08 06:49:32 +0300

    1 try

---
+++ singleton.js
@@ -0,0 +1,27 @@
+var mySingleton = (function(){
+  var instance;
+
+
+
+  function Init(){
+    this.private1 = Math.random();
+    this.private2 = '2';
+  }
+
+
+  function getSingleton(){
+   if (!instance){
+     instance = new Init();
+   }
+   return instance;
+  
+  }
+return {
+getSingleton:getSingleton;
+}
+
+})();
+var getmeasingleton1 = mySingleton.getSingleton();
+var getmeasingleton2 = mySingleton.getSingleton();
+alert(getmeasingleton1.private1);
+alert(getmeasingleton2.private1);
