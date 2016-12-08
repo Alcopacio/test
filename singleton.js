0af8d601ced9bce7b9274a5884bd47700cec189c
Khanas Vasily <alcoapcio@gmail.com>
2016-12-08 06:55:27 +0300

    как должно быть

--- singleton.js
+++ singleton.js
@@ -1,27 +1,53 @@
 var mySingleton = (function(){
   var instance;
 
-
-
-  function Init(){
-    this.private1 = Math.random();
-    this.private2 = '2';
-  }
-
-
-  function getSingleton(){
-   if (!instance){
-     instance = new Init();
-   }
-   return instance;
-  
-  }
-return {
-getSingleton:getSingleton
-}
+function init() {
+ 
+    // Singleton
+ 
+    // Private methods and variables
+    function privateMethod(){
+        console.log( "I am private" );
+    }
+ 
+    var privateVariable = "Im also private";
+ 
+    var privateRandomNumber = Math.random();
+ 
+    return {
+ 
+      // Public methods and variables
+      publicMethod: function () {
+        console.log( "The public can see me!" );
+      },
+ 
+      publicProperty: "I am also public",
+ 
+      getRandomNumber: function() {
+        return privateRandomNumber;
+      }
+ 
+    };
+ 
+  };
+ 
+  return {
+ 
+    // Get the Singleton instance if one exists
+    // or create one if it doesn't
+    getInstance: function () {
+ 
+      if ( !instance ) {
+        instance = init();
+      }
+ 
+      return instance;
+    }
+ 
+  };
 
 })();
-var getmeasingleton1 = mySingleton.getSingleton();
-var getmeasingleton2 = mySingleton.getSingleton();
-alert(getmeasingleton1.private1);
-alert(getmeasingleton2.private1);
+var getmeasingleton1 = mySingleton.getInstance();
+var getmeasingleton2 = mySingleton.getInstance();
+alert(getmeasingleton1.publicProperty);
+alert(getmeasingleton2.privateVariable);
